44f0f91f4d42cb543fae2e449335e7c5d99d1cf2
Juan <jdpelaez68@gmail.com>
2023-09-26 18:58:26 -0500

    Se agrega el listar para cada tipo de servicios

--- backend/app/Http/Controllers/ServiceController.php
+++ backend/app/Http/Controllers/ServiceController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ServiceController extends Controller
 {
-    //
+    public function indexEvents(Request $request)
+    {
+        try {
+            $events = DB::table('services')
+                ->where('services.category_id', 1)
+                ->where('status', true);
+
+            return response()->json([
+                'state' => true,
+                'data' => $events
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function indexSupports(Request $request)
+    {
+        try {
+            $supports = DB::table('services')
+            ->where('services.category_id', 2)
+            ->where('status', true);
+
+            return response()->json([
+                'state' => true,
+                'data' => $supports
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
 }
