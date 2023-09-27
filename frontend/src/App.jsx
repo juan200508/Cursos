7c3fd7dcdcd94a3e912a319c290022990278724b
Yeison Castellanos <yachost@gmail.com>
2023-09-27 02:32:23 -0500

    Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
-//theme
-import "primereact/resources/themes/lara-light-indigo/theme.css";
 
-//core
-import "primereact/resources/primereact.min.css";
-
-import { Login } from "./components/organisms/Login";
 
 function App() {
   return (
-    <>
-    </>
+      <>
+        
+      </>
+    
   );
 }
 
