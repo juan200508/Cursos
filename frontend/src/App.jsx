86570510e79ec5196de8d3b4ae729eaa66dfacc7
Yeison Castellanos <yachost@gmail.com>
2023-09-26 22:13:16 -0500

    Instalacion de Primereact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";
 import './App.css'
 
 function App() {
 
   return (
       <>
+
       </>
   )
 }
