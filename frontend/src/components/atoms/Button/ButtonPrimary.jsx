b42ad58b5c5b235c3cbca86533aef259c47732be
Juan <jdpelaez68@gmail.com>
2023-09-27 08:02:26 -0500

    Se cambia la estructura a monolitico por temas de despliegue

--- frontend/src/components/atoms/Button/ButtonPrimary.jsx
+++
@@ -1,8 +0,0 @@
-// Import the primereact button set
-import {Button} from "primereact/button"
-
-export const ButtonPrimary = (props) => {
-  return (
-    <Button label={props.label} severity={props.severity} className="py-2"/>
-  )
-}
