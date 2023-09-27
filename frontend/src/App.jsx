dda0ad6b38444afa8557152354b06a31dd886b6b
Yeison Castellanos <yachost@gmail.com>
2023-09-26 23:04:13 -0500

    Se eliminaron archivos de estilo css innecesarios y se creó la estructura de carpetas siguiendo la metodología Atomic Design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
     
 //core
 import "primereact/resources/primereact.min.css";
-import './App.css'
+
+import { ButtonPrimary } from "./components/atoms/Button/ButtonPrimary";
 
 function App() {
 
   return (
       <>
-
+        <ButtonPrimary />
       </>
   )
 }
