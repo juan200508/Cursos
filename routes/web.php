6c1513b37bcfbb08383f9283e5f7d2c83819d0b3
Juan <jdpelaez68@gmail.com>
2023-09-27 08:50:57 -0500

    Se agregan las vistas iniciales para trabajo con blade

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('applicant', 'index')->name('applicant.index');
+    Route::get('degrees', 'listDegrees')->name('degrees.list');
+    Route::post('applicant/store', 'store')->name('applicant.store');
+    Route::get('applicant/{id}', 'findApplicant')->name('applicant.find');
+    Route::put('applicant/update', 'updateDetails')->name('applicant.update');
+    Route::put('applicant/disable/{id}', 'disableApplicant')->name('applicant.disable');
+    Route::put('applicant/enable/{id}', 'enableApplicant')->name('applicant.enable');
+    Route::post('administrator/store', 'storeAdministrator')->name('administrator.store');
+});
+
+Route::controller(ServiceController::class)->group(function () {
+    Route::get('categories', 'listCategories')->name('cartegories.list');
+    Route::get('services/events', 'indexEvents')->name('services.events');
+    Route::get('services/supports', 'indexSupports')->name('services.supports');
+    Route::post('services/store', 'store')->name('services.store');
+    Route::get('services/{id}', 'findService')->name('services.find');
+    Route::put('services/update', 'update')->name('services.update');
+    Route::put('services/disable/{id}', 'disable')->name('services.disable');
+    Route::put('services/enable/{id}', 'enable')->name('services.enable');
+});
+
+// Routes for views
+
+Route::get('users/index', function(){
+    return view('users.index');
+});
+
+
